61284e72fdce02d87911ddd9fd6a3913694a7313
aka3758 <1334832865@qq.com>
2022-01-04 00:35:24 +0800

    更新

--- test.cpp
+++ test.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+class Base
+{
+public:
+    Base()
+    {
+        value1 = 1;
+    }
+
+    Base(int value) : Base()  //委托构造函数
+    {
+        value2 = value;
+    }
+
+    int value1;
+    int value2;
+};
+
 int main()
 {
-    auto x = 1;
-    auto y = 2;
+    Base b(2);
+    cout << b.value1 << endl;
+    cout << b.value2 << endl;
     return 0;
 }
